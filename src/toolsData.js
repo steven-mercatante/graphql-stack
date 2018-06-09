69bf7d6e0cf61fb7b747c133c214d80acea8cf65
Steven Mercatante <steven.mercatante@gmail.com>
2018-06-09 17:09:56 -0400

    Comment out server data in toolsData

--- src/toolsData.js
+++ src/toolsData.js
@@ -89,20 +89,20 @@ export default {
       }
     ]
   },
-  server: {
-    name: "Server",
-    description:
-      "These frameworks are language-specific and are usually the ones responsible for fetching data.",
-    tools: [
-      {
-        name: "Express",
-        description: "",
-        url: "https://expressjs.com/",
-        github: "https://github.com/expressjs/expressjs.com",
-        language: JavaScript
-      }
-    ]
-  },
+  // server: {
+  //   name: "Server",
+  //   description:
+  //     "These frameworks are language-specific and are usually the ones responsible for fetching data.",
+  //   tools: [
+  //     {
+  //       name: "Express",
+  //       description: "",
+  //       url: "https://expressjs.com/",
+  //       github: "https://github.com/expressjs/expressjs.com",
+  //       language: JavaScript
+  //     }
+  //   ]
+  // },
   dbProxy: {
     name: "Database Proxy",
     description:
