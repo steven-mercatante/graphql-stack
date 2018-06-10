8cca7eb92fe61005577058cfebde134aab806fc4
Steven Mercatante <steven.mercatante@gmail.com>
2018-06-10 10:02:50 -0400

    Add quotation marks to GraphQL explanation

--- src/App.js
+++ src/App.js
@@ -20,13 +20,15 @@ class App extends Component {
           tools and how they fit in to the overall ecosystem.
         </p>
         <h3>In case you don't know what GraphQL is...</h3>
-        <p className="description">
+        <p className="explanation">
           GraphQL is a query language for APIs and a runtime for fulfilling
           those queries with your existing data. GraphQL provides a complete and
           understandable description of the data in your API, gives clients the
           power to ask for exactly what they need and nothing more, makes it
           easier to evolve APIs over time, and enables powerful developer tools.
-          <br />- <a href="https://graphql.org/">Official GraphQL site</a>
+        </p>
+        <p className="credit">
+          - <a href="https://graphql.org/">Official GraphQL site</a>
         </p>
         <div className="sectionsList">
           <div className="line" />
