20a9a8cbcca064b70f093973c1f5f9cabb841ec8
Steven Mercatante <steven.mercatante@gmail.com>
2018-06-10 19:41:17 -0400

    Text spacing for header

--- src/App.js
+++ src/App.js
@@ -11,27 +11,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          The{" "}
-          <img src={graphqlLogo} alt="GraphQL logo" className="graphqlLogo" />
-          <span className="pink">GraphQL</span> Stack
-        </h1>
-        <p className="intro">
-          GraphQL is exciting, but its ecosystem can be confusing because of all
-          the tools available. This chart is meant to help explain the various
-          tools and how they fit in to the overall ecosystem.
-        </p>
-        <h3>In case you don't know what GraphQL is...</h3>
-        <p className="explanation">
-          GraphQL is a query language for APIs and a runtime for fulfilling
-          those queries with your existing data. GraphQL provides a complete and
-          understandable description of the data in your API, gives clients the
-          power to ask for exactly what they need and nothing more, makes it
-          easier to evolve APIs over time, and enables powerful developer tools.
-        </p>
-        <p className="credit">
+        <header>
+          <h1>
+            The{" "}
+            <img src={graphqlLogo} alt="GraphQL logo" className="graphqlLogo" />
+            <span className="pink">GraphQL</span> Stack
+          </h1>
+          <p className="intro">
+            GraphQL is exciting, but its ecosystem can be confusing because of
+            all the tools available. This chart is meant to help explain the
+            various tools and how they fit in to the overall ecosystem.
+          </p>
+          <h3>In case you don't know what GraphQL is...</h3>
+          <p className="explanation">
+            GraphQL is a query language for APIs and a runtime for fulfilling
+            those queries with your existing data. GraphQL provides a complete
+            and understandable description of the data in your API, gives
+            clients the power to ask for exactly what they need and nothing
+            more, makes it easier to evolve APIs over time, and enables powerful
+            developer tools.
+          </p>
           - <a href="https://graphql.org/">Official GraphQL site</a>
-        </p>
+        </header>
         <div className="sectionsList">
           <div className="line" />
           <Section type="client" data={toolsData["client"]} />
